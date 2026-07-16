f1b31bc7796154b6a7ee4a760babb126daa5057f
Baptiste Louërat <baptiste.louerat.pro@outlook.fr>
2026-07-16 15:07:20 +0200

    Fix: J'ai fix pour que graddle utilise Java17 et pas 21 J'ai toujours le message d'erreur sur mon kotlin du app build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,15 +32,13 @@ android {
 }
 
 dependencies {
+    // UI et bases Android (utilisation du catalogue de versions)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 
-// Carte Gratuite (Alternative à Google Maps)
+    // Carte Gratuite (Alternative à Google Maps)
     implementation("org.osmdroid:osmdroid-android:6.1.18")
 
     // Web Service REST
@@ -52,8 +50,11 @@ dependencies {
     implementation("androidx.room:room-runtime:$roomVersion")
     annotationProcessor("androidx.room:room-compiler:$roomVersion")
 
-    // UI et Tests
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // Tests (utilisation du catalogue de versions)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    // Si tu as bien ajouté libs.test.rules à l'étape précédente, tu peux aussi l'ajouter ici
+    // androidTestImplementation(libs.test.rules)
 }
